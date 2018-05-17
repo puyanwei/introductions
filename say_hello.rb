fc83d7b24d2dc6e23f0334525786d88e90143286
Adam Romano <manoadamro.imp@gmail.com>
2018-05-17 16:37:50 +0100

    proper commit message (after all this time)
    Gentle Gregory Fish - a short story

--- say_hello.rb
+++ say_hello.rb
@@ -9,3 +9,34 @@ puts "Hello Adam I'm creepily watching you!"
 puts "Hello Adam I'm creepily watching you!"
 
 puts "I'm ok with it. :)"
+
+puts "
+
+Gentle Gregory Fish
+A Short Story
+by InadequateMonkey
+Gregory Fish had always loved damp Upper Boggington with its joyous, jealous jungle. It was a place where he felt delighted.
+
+He was a gentle, sinister, brandy drinker with curvy lips and fat fingers. His friends saw him as an aggressive, angry animal. Once, he had even helped a tasty baby flamingo cross the road. That's the sort of man he was.
+
+Gregory walked over to the window and reflected on his hilly surroundings. The sleet rained like partying mice.
+
+Then he saw something in the distance, or rather someone. It was the figure of Ocean Blackman. Ocean was a wild queen with slimy lips and brunette fingers.
+
+Gregory gulped. He was not prepared for Ocean.
+
+As Gregory stepped outside and Ocean came closer, he could see the open smile on her face.
+
+Ocean gazed with the affection of 2611 bold magnificent maggots. She said, in hushed tones, "I love you and I want revenge."
+
+Gregory looked back, even more sparkly and still fingering the tiny kettle. "Ocean, Is that real leather," he replied.
+
+They looked at each other with irritable feelings, like two frightened, fierce foxes talking at a very violent wedding, which had trance music playing in the background and two adorable uncles laughing to the beat.
+
+Gregory regarded Ocean's slimy lips and brunette fingers. "I feel the same way!" revealed Gregory with a delighted grin.
+
+Ocean looked calm, her emotions blushing like a good, great guillotine.
+
+Then Ocean came inside for a nice glass of brandy.
+
+THE END"
